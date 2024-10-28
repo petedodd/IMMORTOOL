b073859652d777007489400a28c48fe344022b0a
PJ Dodd <p.j.dodd@sheffield.ac.uk>
2024-10-28 09:20:56 +0000

    tidying & starting vignette

--- R/app_ui.R
+++ R/app_ui.R
@@ -62,7 +62,8 @@ app_ui <- function(request) {
                                         uiOutput("TTEfracinput"))
                                ),
                                actionButton("do", "Try to fit!"),
-                               textOutput('fits')
+                               ## textOutput('fits'),
+                               h3("(See next tab for fit)")
                                ),
                       tabPanel("Time-to-event distributions", plotOutput(outputId = "FitPlot")),
                       tabPanel("Run simulation",
@@ -73,6 +74,7 @@ app_ui <- function(request) {
                                               "Fit values" = "fvl")),
                                h2("\n"),
                                actionButton("runsim", "Run simulation!"),
+                               h3("(See next tab for results)"),
                                h2("\n"),
                                ## Input: Slider for End time ----
                                sliderInput(inputId = "T.max",
@@ -120,75 +122,10 @@ app_ui <- function(request) {
 
                                ),
                       tabPanel("Simulation results", tableOutput("tableA1"))
-                      ## tabPanel("Distribution stats", tableOutput('dists'))
-                      ## ,
-                      ## tabPanel("Time-to-event distributions", plotOutput(outputId = "TMPlot")),
-                      ## tabPanel("Time-to-event distributions", plotOutput(outputId = "distPlot")),
-                      ## tabPanel("Output Plot", plotOutput(outputId = "resultPlot"))#,
-                      ## tabPanel("Rates, method a", tableOutput('tableA1')),
-                      ## tabPanel("Rate ratio, method a", tableOutput('tableA2')),
-                      ## tabPanel("Rates, method b", tableOutput('tableB1')),
-                      ## tabPanel("Rate ratio, method b", tableOutput('tableB2'))
+
                       )
         )
       )
-
-   ##    ## Sidebar layout with input and output definitions ----
-   ##    sidebarLayout(
-
-   ##      ## Sidebar panel for inputs ----
-   ##      ## sidebarPanel(
-
-   ##      ##   ## Input: Slider for End time ----
-   ##      ##   sliderInput(inputId = "T.max",
-   ##      ##               label = "Follow-up time:",
-   ##      ##               min = 1,
-   ##      ##               max = 100,
-   ##      ##               value = 90),
-
-   ##      ##   ## Input: Slider for exposure ----
-   ##      ##   sliderInput(inputId = "L.e",
-   ##      ##               label = "Exposure scale parameter:",
-   ##      ##               min = 0.001,
-   ##      ##               max = 500,
-   ##      ##               value = 10.6),
-   ##      ##   sliderInput(inputId = "k.e",
-   ##      ##               label = "Exposure shape parameter:",
-   ##      ##               min = -1,
-   ##      ##               max = 20,
-   ##      ##               value = 2.3),
-
-   ##      ##   ## Input: Slider for death ----
-   ##      ##   sliderInput(inputId = "L.d",
-   ##      ##               label = "Death scale parameter:",
-   ##      ##               min = 0.001,
-   ##      ##               max = 500,
-   ##      ##               value = 428),
-   ##      ##   sliderInput(inputId = "k.d",
-   ##      ##               label = "Death shape parameter:",
-   ##      ##               min = -1,
-   ##      ##               max = 20,
-   ##      ##               value = 0.5),
-
-   ##      ##   ## Input: Time for landmark analysis ---
-   ##      ##   sliderInput(inputId = "T.landmark",
-   ##      ##               label = "Time for landmark analysis:",
-   ##      ##               min = 1,
-   ##      ##               max = 100,
-   ##      ##               value = 3)
-
-   ##      ##   ## ## Input: Slider for death ----
-   ##      ##   ## sliderInput(inputId = "L.l",
-   ##      ##   ##             label = "LTFU scale parameter:",
-   ##      ##   ##             min = 2,
-   ##      ##   ##             max = 1000,
-   ##      ##   ##             value = 5),
-   ##      ##   ## sliderInput(inputId = "k.l",
-   ##      ##   ##             label = "LTFU shape parameter:",
-   ##      ##   ##             min = -1,
-   ##      ##   ##             max = 20,
-   ##      ##   ##             value = 1)
-   ##      ## ),
 
   )
 }
